d027bb51d5b24e9fd9185799e66d29e116365257
AbolfazlMohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2023-07-24 13:02:45 +0330

    Add <Calculating_Multiples> To <Source/PMath.py>

--- Source/PMath.py
+++ Source/PMath.py
@@ -13,3 +13,24 @@ def Calculating_Divisors(Number: int) -> tuple:
                 Divisors.append(Counter)
 
     return tuple(Divisors)
+
+
+def Calculating_Multiples(Number: int, From: int, To: int) -> tuple:
+    """This Function Calculates Multiples Of <Number> Argument From <From> Argument To <To Argument>"""
+    Multiples = list()
+    if (Number == 0):
+        raise "The Input Argument Does Not Have To Be Zero"
+    elif (Number > 0):
+        if (From > To):
+            raise "The <From> Argument Does Not Have To Be Greater Than <To> Argument"
+        else:
+            for Counter in range(From, (To + 1)):
+                Multiples.append((Number * Counter))
+    elif (Number < 0):
+        if (From > To):
+            raise "The <From> Argument Does Not Have To Be Greater Than <To> Argument"
+        else:
+            for Counter in range(From, (To + 1)):
+                Multiples.append((Number * Counter))
+
+    return tuple(Multiples)
