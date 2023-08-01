f36d3c0a12626be8d80944bba2dce1c735b03aa2
AbolfazlMohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2023-08-01 22:51:32 +0330

    Edit <Calculating_Divisors> From <Source/PMath.py>

--- Source/PMath.py
+++ Source/PMath.py
@@ -2,7 +2,7 @@ def Calculating_Divisors(Number: int) -> tuple:
     """This Function Calculates Divisors Of <Number> Argument """
     Divisors = list()
     if (Number == 0):
-        raise "The Input Argument Does Not Have To Be Zero"
+        raise "The Argument <Number> Does Not Have To Be Zero"
     elif (Number > 0):
         for Counter in range(1, (Number + 1)):
             if ((Number % Counter) == 0):
